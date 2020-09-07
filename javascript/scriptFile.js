c205d55fda5d045fe9a867e8304679c1e7d4e952
mahdi abdulkareem <mahdiimad64@gmail.com>
2020-09-07 10:41:52 -0700

    changes from abd

--- javascript/scriptFile.js
+++ javascript/scriptFile.js
@@ -1,9 +1,7 @@
-// localStorage.clear();
-
 let toDo;
 if (localStorage.getItem("toDo") !== null) {
   toDo = localStorage.getItem("toDo");
-  // alert(toDo)
+
   document.getElementById("todo").textContent = toDo;
 } else {
   toDo = 0;
@@ -31,24 +29,15 @@ if (localStorage.getItem("tasks") !== null) {
   id = 0;
 }
 
-// for (let i=0; i <= localStorage.length-1; i++)  {
-//   console.log( localStorage.key( i ) + " " + localStorage.getItem( localStorage.key( i ) ) + " /" + JSON.parse(localStorage.getItem("tasks")).length);
-
-// }
-
 document.getElementById("addButton").onclick = function () {
   const task = document.getElementById("taskText").value;
   const assignee = document.getElementById("assigneeText").value;
   taskArr.push({ id, task, assignee });
   window.localStorage.setItem("tasks", JSON.stringify(taskArr));
-
-  // const parentNode = node.parentNode;
   id += 1;
   addTask(id, task, assignee);
   // reset input
-  document.getElementById("taskText").value = "";
-  document.getElementById("assigneeText").value = "";
-
+  resetInput();
   //increment todo
   toDo = parseInt(toDo);
   toDo += 1;
@@ -57,25 +46,30 @@ document.getElementById("addButton").onclick = function () {
   window.localStorage.setItem("done", done);
 };
 
-function doneFun(x) {
+function resetInput(){
+  document.getElementById("taskText").value = "";
+  document.getElementById("assigneeText").value = "";
+
+}
+function handleTaskDoneClick(event) {
   done = parseInt(done);
   toDo = parseInt(toDo);
   done += 1;
   toDo -= 1;
 
-  // console.log(x.parentNode.parentNode.id);
-  if (doneArr.includes(x.parentNode.parentNode.id) === false) {
-    //    alert(1);
-    doneArr.push(x.parentNode.parentNode.id);
+ 
+  if (doneArr.includes(event.parentNode.parentNode.id) === false) {
+  
+    doneArr.push(event.parentNode.parentNode.id);
     document.getElementById("done").textContent = done;
     document.getElementById("todo").textContent = toDo;
-    x.style.color = "green";
+    event.style.color = "green";
   } else alert("you done this");
 }
 
 function deleteFun(y) {
-  const x = confirm("Are you sure Delete This Task ?");
-  if (x == true) {
+  const confirmationAnswer = confirm("Are you sure Delete This Task ?");
+  if (confirmationAnswer == true) {
     document.getElementById(y.parentNode.parentNode.id).remove();
     toDo -= 1;
     document.getElementById("done").textContent = done;
@@ -88,16 +82,13 @@ function deleteFun(y) {
 function searchFun() {
   const input = document.getElementById("search").value;
   const node = document.querySelectorAll(".task");
-  // for(let i = 2 ; i  < node.childElementCount ; i++){
-  //   console.log(node.firstElementChild.nextElementSibling);
-  // }
-
+ 
   for (let i = 0; i < node.length; i++) {
     //  console.log(node[i].childNodes[3].childNodes[1].textContent);
     let task = node[i].querySelector(".taskDemo").textContent;
     let assignee = node[i].querySelector(".assigneeDemo").textContent;
 
-    // console.log(task.indexOf(input) + "   " + assignee.indexOf(input));
+  
     if (task.indexOf(input) > -1 || assignee.indexOf(input) > -1) {
       node[i].style.display = "";
     } else {
