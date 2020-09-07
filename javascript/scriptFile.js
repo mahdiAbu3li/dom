8d497881561774018600b9df2a447f79d8060c36
mahdi abdulkareem <mahdiimad64@gmail.com>
2020-09-07 10:58:55 -0700

    remove comment and dead code

--- javascript/scriptFile.js
+++ javascript/scriptFile.js
@@ -82,13 +82,9 @@ function deleteFun(y) {
 function searchFun() {
   const input = document.getElementById("search").value;
   const node = document.querySelectorAll(".task");
- 
   for (let i = 0; i < node.length; i++) {
-    //  console.log(node[i].childNodes[3].childNodes[1].textContent);
     let task = node[i].querySelector(".taskDemo").textContent;
     let assignee = node[i].querySelector(".assigneeDemo").textContent;
-
-  
     if (task.indexOf(input) > -1 || assignee.indexOf(input) > -1) {
       node[i].style.display = "";
     } else {
@@ -98,9 +94,8 @@ function searchFun() {
 }
 
 function addTask(id, task, assignee) {
-  const node = document.querySelector("#parentTask"); //.cloneNode(true);
+  const node = document.querySelector("#parentTask"); 
   const newNode = document.getElementById("task").content.firstElementChild.cloneNode(true);
-
   newNode.id = id;
   node.appendChild(newNode);
   document.getElementById(id).querySelector(".taskDemo").innerText = task;
